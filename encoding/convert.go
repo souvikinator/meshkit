7b620668cfe42b1a70cc4c55eddcc7cfc42d03c1
Souvik Kar Mahapatra <souvikat001@gmail.com>
2025-02-11 18:19:36 +0530

    1. Unmarshall the data to yaml.Node instead of map[string]interface{} to preserver field order. 2. Recursively reset style for each node to get actual yaml output
    Signed-off-by: Souvik Kar Mahapatra <souvikat001@gmail.com>

--- encoding/convert.go
+++ encoding/convert.go
@@ -1,15 +1,42 @@
 package encoding
 
 import (
+	"bytes"
+	"fmt"
+
 	"gopkg.in/yaml.v3"
 )
 
+// ToYaml converts input JSON (or YAML) data into YAML while preserving key order.
+// It unmarshalls data into yaml.Node instead of map[string]interface{} because
+// maps do not preserve field order.
 func ToYaml(data []byte) ([]byte, error) {
-	var out map[string]interface{}
+	var out yaml.Node
 	err := Unmarshal(data, &out)
 	if err != nil {
 		return nil, err
 	}
 
-	return yaml.Marshal(out)
+	if len(out.Content) == 0 {
+		return nil, ErrDecodeYaml(fmt.Errorf("No content found in the yaml file."))
+	}
+
+	// Recursively set the style of nodes to block style for readable formatting.
+	setBlockStyle(out.Content[0])
+
+	var buf bytes.Buffer
+	enc := yaml.NewEncoder(&buf)
+	enc.SetIndent(2)
+	err = enc.Encode(out.Content[0])
+	return buf.Bytes(), err
+}
+
+// setBlockStyle recursively resets the style of the node in yaml.Node and its children to block style.
+// In simple terms, it makes the output print on multiple indented lines
+// instead of a single inline line (which looks like JSON).
+func setBlockStyle(n *yaml.Node) {
+	n.Style = 0
+	for _, child := range n.Content {
+		setBlockStyle(child)
+	}
 }
